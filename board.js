6cb24d19a84cb886cf380d8555503f00a8320617
rahat-srijon <rahat1999srijon@gmail.com>
2023-03-27 10:38:23 +0600

    Board initialization

---
+++ board.js
@@ -0,0 +1,68 @@
+function initBoard(){
+    grid=[
+      [
+        new Rook('black',[0,0]),
+        new Knight('black',[0,1]),
+        new Bishop('black',[0,2]),
+        new Queen('black',[0,3]),
+        new King('black',[0,4]),
+        new Bishop('black',[0,5]),
+        new Knight('black',[0,6]),
+        new Rook('black',[0,7]),
+      ],
+      [
+        new Pawn('black',[1,0]),
+        new Pawn('black',[1,1]),
+        new Pawn('black',[1,2]),
+        new Pawn('black',[1,3]),
+        new Pawn('black',[1,4]),
+        new Pawn('black',[1,5]),
+        new Pawn('black',[1,6]),
+        new Pawn('black',[1,7]),
+      ],
+      [null,null,null,null,null,null,null,null],
+      [null,null,null,null,null,null,null,null],
+      [null,null,null,null,null,null,null,null],
+      [null,null,null,null,null,null,null,null],
+      [
+        new Pawn('white',[6,0]),
+        new Pawn('white',[6,1]),
+        new Pawn('white',[6,2]),
+        new Pawn('white',[6,3]),
+        new Pawn('white',[6,4]),
+        new Pawn('white',[6,5]),
+        new Pawn('white',[6,6]),
+        new Pawn('white',[6,7]),
+      ],
+      [
+        new Rook('white',[7,0]),
+        new Knight('white',[7,1]),
+        new Bishop('white',[7,2]),
+        new Queen('white',[7,3]),
+        new King('white',[7,4]),
+        new Bishop('white',[7,5]),
+        new Knight('white',[7,6]),
+        new Rook('white',[7,7]),
+      ],
+    ];
+}
+function board(){
+    sz=min(windowWidth,windowHeight);
+    scale=sz/8;
+    startX=max(0,windowWidth/2-sz/2);
+    startY=max(0,windowHeight/2-sz/2);
+    for(let i=0;i<8;i++){
+        for(let j=0;j<8;j++){
+            let color=i%2==j%2;
+            fill(color*255,color*255,color*255);
+            rect(startX+i*scale,startY+j*scale,scale,scale);
+        }
+    }
+    for(let i=0;i<8;i++){
+        for(let j=0;j<8;j++){
+            if(grid[i][j]!=null){
+                grid[i][j].draw(startX+j*scale,startY+i*scale);
+            }
+        }
+    }
+}
---
+++ piece.js
@@ -0,0 +1,80 @@
+class Piece{
+    constructor(color,position){
+        this.color=color;
+        this.position=position;
+    }
+    getValidMoves(board){}
+    isMoveLegal(board){}
+    move(position,board){}
+    draw(){}
+}
+class Pawn extends Piece{
+    constructor(color,position){
+        super(color,position);
+    }
+    getValidMoves(board){}
+    isMoveLegal(board){}
+    move(position,board){}
+    draw(x,y){
+        if(this.color=='black')fill(255,100,100);
+        else if(this.color=='white')fill(100,100,255);
+        triangle(x+scale*0.50,y+scale/2-scale*0.3,x+scale*0.75,y+scale*.75,x+scale*0.25,y+scale*.75);
+        circle(x+scale/2,y+scale/2-scale*0.2,scale/3);
+        rect(x+scale*0.10,y+scale/2+scale*0.2,scale*0.8,scale*0.20,1000,1000,1000,1000);
+    }
+}
+class Rook extends Piece{
+    constructor(color,position){
+        super(color,position);
+    }
+    getValidMoves(board){}
+    isMoveLegal(board){}
+    move(position,board){}
+    draw(x,y){
+        if(this.color=='black')fill(255,100,100);
+        else if(this.color=='white')fill(100,100,255);
+        triangle(x+scale*0.50,y+scale*0.50-scale*0.3,x+scale*0.75,y+scale*0.75,x+scale*0.25,y+scale*0.75);
+        rect(x+scale*0.20,y+scale*0.20,scale*0.15,scale*0.20,0,0,scale*0.10,scale*0.10);
+        rect(x+scale*0.65,y+scale*0.20,scale*0.15,scale*0.20,0,0,scale*0.10,scale*0.10);
+        rect(x+scale*0.20,y+scale*0.30,scale*0.6,scale*0.20,0,0,scale*0.10,scale*0.10);
+        rect(x+scale*0.10,y+scale/2+scale*0.2,scale*0.8,scale*0.20,100,100,100,100);
+    }
+}
+class Knight extends Piece{
+    constructor(color,position){
+        super(color,position);
+    }
+    getValidMoves(board){}
+    isMoveLegal(board){}
+    move(position,board){}
+    draw(x,y){
+        
+    }
+}
+class Bishop extends Piece{
+    constructor(color,position){
+        super(color,position);
+    }
+    getValidMoves(board){}
+    isMoveLegal(board){}
+    move(position,board){}
+    draw(){}
+}
+class Queen extends Piece{
+    constructor(color,position){
+        super(color,position);
+    }
+    getValidMoves(board){}
+    isMoveLegal(board){}
+    move(position,board){}
+    draw(){}
+}
+class King extends Piece{
+    constructor(color,position){
+        super(color,position);
+    }
+    getValidMoves(board){}
+    isMoveLegal(board){}
+    move(position,board){}
+    draw(){}
+}
