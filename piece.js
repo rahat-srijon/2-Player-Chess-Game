0e26f8557865472557629b3132f4bfc41bcb23bf
rahat-srijon <rahat1999srijon@gmail.com>
2023-03-27 21:05:10 +0600

    Pieces Updated

--- piece.js
+++ piece.js
@@ -1,83 +1,178 @@
 class Piece{
-    constructor(color,position){
-        this.color=color;
-        this.position=position;
-    }
-    getValidMoves(board){}
-    isMoveLegal(board){}
-    move(position,board){}
-    draw(){}
+	constructor(color,position){
+		this.color=color;
+		this.position=position;
+	}
+	getValidMoves(board){}
+	isMoveLegal(board){}
+	move(position,board){}
+	draw(){}
 }
 class Pawn extends Piece{
-    constructor(color,position){
-        super(color,position);
-    }
-    getValidMoves(board){}
-    isMoveLegal(board){}
-    move(position,board){}
-    draw(x,y){
-        if(this.color=='black')fill(255,100,100);
-        else if(this.color=='white')fill(100,100,255);
-        triangle(x+scale*0.50,y+scale/2-scale*0.3,x+scale*0.75,y+scale*.75,x+scale*0.25,y+scale*.75);
-        circle(x+scale/2,y+scale/2-scale*0.2,scale/3);
-        rect(x+scale*0.10,y+scale/2+scale*0.2,scale*0.8,scale*0.20,1000,1000,1000,1000);
-    }
+	constructor(color,position){
+		super(color,position);
+	}
+	getValidMoves(board){}
+	isMoveLegal(board){}
+	move(position,board){}
+	draw(x,y){
+		if(this.color=='black')fill(255,100,100);
+		else if(this.color=='white')fill(100,100,255);
+		triangle(
+			x+scale*0.50,y+scale*0.20,
+			x+scale*0.75,y+scale*0.75,
+			x+scale*0.25,y+scale*.75
+			);
+		circle(x+scale*0.50,y+scale*0.50-scale*0.2,scale/3);
+		rect(
+			x+scale*0.10,y+scale*0.7,
+			scale*0.8,scale*0.20,
+			1000,1000,1000,1000
+			);
+	}
 }
 class Rook extends Piece{
-    constructor(color,position){
-        super(color,position);
-    }
-    getValidMoves(board){}
-    isMoveLegal(board){}
-    move(position,board){}
-    draw(x,y){
-        if(this.color=='black')fill(255,100,100);
-        else if(this.color=='white')fill(100,100,255);
-        triangle(x+scale*0.50,y+scale*0.50-scale*0.3,x+scale*0.75,y+scale*0.75,x+scale*0.25,y+scale*0.75);
-        rect(x+scale*0.20,y+scale*0.20,scale*0.15,scale*0.20,0,0,scale*0.10,scale*0.10);
-        rect(x+scale*0.65,y+scale*0.20,scale*0.15,scale*0.20,0,0,scale*0.10,scale*0.10);
-        rect(x+scale*0.20,y+scale*0.30,scale*0.6,scale*0.20,0,0,scale*0.10,scale*0.10);
-        rect(x+scale*0.10,y+scale/2+scale*0.2,scale*0.8,scale*0.20,100,100,100,100);
-    }
+	constructor(color,position){
+		super(color,position);
+	}
+	getValidMoves(board){}
+	isMoveLegal(board){}
+	move(position,board){}
+	draw(x,y){
+		if(this.color=='black')fill(255,100,100);
+		else if(this.color=='white')fill(100,100,255);
+		triangle(
+			x+scale*0.50,y+scale*0.20,
+			x+scale*0.75,y+scale*0.75,
+			x+scale*0.25,y+scale*0.75
+			);
+		rect(
+			x+scale*0.20,y+scale*0.20,
+			scale*0.15,scale*0.20,
+			0,0,scale*0.10,scale*0.10
+			);
+		rect(
+			x+scale*0.65,y+scale*0.20,
+			scale*0.15,scale*0.20,
+			0,0,scale*0.10,scale*0.10
+			);
+		rect(
+			x+scale*0.20,y+scale*0.30,
+			scale*0.6,scale*0.20,
+			0,0,scale*0.10,scale*0.10
+			);
+		rect(
+			x+scale*0.10,y+scale*0.7,
+			scale*0.8,scale*0.20,
+			100,100,100,100
+			);
+	}
 }
 class Knight extends Piece{
-    constructor(color,position){
-        super(color,position);
-    }
-    getValidMoves(board){}
-    isMoveLegal(board){}
-    move(position,board){}
-    draw(x,y){
-        triangle(x+scale*0.35,y+scale*0.50-scale*0.3,x+scale*0.75,y+scale*0.75,x+scale*0.25,y+scale*0.75);
-        triangle(x+scale*0.35,y+scale*0.40-scale*0.3,x+scale*0.75,y+scale*0.50,x+scale*0.25,y+scale*0.40);
-        triangle(x+scale*0.50,y+scale*0.40-scale*0.3,x+scale*0.85,y+scale*0.50,x+scale*0.25,y+scale*0.30);
-        rect(x+scale*0.10,y+scale/2+scale*0.2,scale*0.8,scale*0.20,100,100,100,100);
-    }
+	constructor(color,position){
+		super(color,position);
+	}
+	getValidMoves(board){}
+	isMoveLegal(board){}
+	move(position,board){}
+	draw(x,y){
+		triangle(
+			x+scale*0.25, y+scale*0.7,
+			x+scale*0.60, y+scale*0.7,
+			x+scale*0.60, y+scale*0.2
+			);
+		triangle(
+			x+scale*0.25, y+scale*0.35,
+			x+scale*0.50, y+scale*0.35,
+			x+scale*0.50, y+scale*0.15
+			);
+		triangle(
+			x+scale*0.70, y+scale*0.55,
+			x+scale*0.50, y+scale*0.55,
+			x+scale*0.50, y+scale*0.15
+			);
+		triangle(
+			x+scale*0.75, y+scale*0.7,
+			x+scale*0.40, y+scale*0.7,
+			x+scale*0.40, y+scale*0.3
+			);
+		rect(
+			x+scale*0.10,y+scale*0.7,
+			scale*0.8,scale*0.20,
+			100,100,100,100
+			);
+	}
 }
 class Bishop extends Piece{
-    constructor(color,position){
-        super(color,position);
-    }
-    getValidMoves(board){}
-    isMoveLegal(board){}
-    move(position,board){}
-    draw(){}
+	constructor(color,position){
+		super(color,position);
+	}
+	getValidMoves(board){}
+	isMoveLegal(board){}
+	move(position,board){}
+	draw(x,y){
+		if(this.color=='black')fill(255,100,100);
+		else if(this.color=='white')fill(100,100,255);
+		triangle(
+			x+scale*0.50,y+scale*0.20,
+			x+scale*0.75,y+scale*0.75,
+			x+scale*0.25,y+scale*0.75
+			);
+		circle(x+scale*0.50,y+scale*0.3,scale/3);
+		rect(
+			x+scale*0.25,y+scale*0.4,
+			scale*0.5,scale*0.10,
+			100,100,100,100
+			);
+		circle(x+scale*0.50,y+scale*0.50-scale*0.35,scale/6);
+		rect(
+			x+scale*0.10,y+scale*0.70,
+			scale*0.8,scale*0.20,
+			100,100,100,100
+			);
+	}
+
 }
 class Queen extends Piece{
-    constructor(color,position){
-        super(color,position);
-    }
-    getValidMoves(board){}
-    isMoveLegal(board){}
-    move(position,board){}
-    draw(){}
+	constructor(color,position){
+		super(color,position);
+	}
+	getValidMoves(board){}
+	isMoveLegal(board){}
+	move(position,board){}
+	draw(x,y){
+		if(this.color=='black')fill(255,100,100);
+		else if(this.color=='white')fill(100,100,255);
+		triangle(
+			x+scale*0.50,y+scale*0.20,
+			x+scale*0.75,y+scale*0.75,
+			x+scale*0.25,y+scale*0.75
+			);
+		circle(x+scale*0.60,y+scale*0.3,scale*.30);
+		circle(x+scale*0.40,y+scale*0.3,scale*.30);
+		rect(
+			x+scale*0.40,y+scale*0.20,
+			scale*0.2,scale*0.20
+			);
+		rect(
+			x+scale*0.25,y+scale*0.4,
+			scale*0.5,scale*0.10,
+			100,100,100,100
+			);
+		circle(x+scale*0.50,y+scale*0.50-scale*0.35,scale/6);
+		rect(
+			x+scale*0.10,y+scale*0.70,
+			scale*0.8,scale*0.20,
+			100,100,100,100
+			);
+	}
 }
 class King extends Piece{
-    constructor(color,position){
-        super(color,position);
-    }
-    getValidMoves(board){}
-    isMoveLegal(board){}
-    move(position,board){}
-    draw(){}
+	constructor(color,position){
+		super(color,position);
+	}
+	getValidMoves(board){}
+	isMoveLegal(board){}
+	move(position,board){}
+	draw(){}
 }
--- sketch.js
+++ sketch.js
@@ -1,16 +1,16 @@
 let grid=[];
 let started=0;
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  textAlign(CENTER,CENTER);
-  document.body.style.overflow='hidden';
+	createCanvas(windowWidth,windowHeight);
+	textAlign(CENTER,CENTER);
+	document.body.style.overflow='hidden';
 }
 var scale;
 function draw() {
-  background(40);
-  if(started==0){
-    initBoard();
-    started=1;
-  }
-  board();
+	background(40);
+	if(started==0){
+		initBoard();
+		started=1;
+	}
+	board();
 }
